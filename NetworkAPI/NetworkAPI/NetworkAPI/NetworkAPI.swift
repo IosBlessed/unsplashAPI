453fedf564e9feccf113831f5f68cc9d6760330c
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-06-08 13:39:00 +0300

    Prepared structure for swift package manager

--- NetworkAPI/NetworkAPI/NetworkAPI/NetworkAPI.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkAPI.swift
-//  NetworkAPI
-//
-//  Created by Никита Данилович on 08.06.2023.
-//
-
-import Foundation
--- NetworkAPI/NetworkAPI/NetworkAPI/NetworkProtocol.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkProtocol.swift
-//  NetworkAPI
-//
-//  Created by Никита Данилович on 08.06.2023.
-//
-
-import Foundation
